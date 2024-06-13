8b881b98abdfc1a3aec5fd06e3a76c889c96c337
Juraj Michalek <juraj.michalek@gmail.com>
2024-06-13 06:11:36 +0200

    load palette from PCX

--- components/HelloSDL/pcxload.c
+++ components/HelloSDL/pcxload.c
@@ -22,46 +22,48 @@
 #include "pcxload.h"
 #include "video.h"
 
-void JE_loadPCX( const char *file ) // this is only meant to load tshp2.pcx
+void JE_loadPCX(const char *file) // this is only meant to load tshp2.pcx
 {
-	Uint8 *s = (Uint8 *)VGAScreen->pixels; /* 8-bit specific */
-	
-	FILE *f = dir_fopen_die(data_dir(), file, "rb");
-	
-	efseek(f, -769, SEEK_END);
-	
+    Uint8 *s = (Uint8 *)VGAScreen->pixels; /* 8-bit specific */
+    
+    FILE *f = dir_fopen_die(data_dir(), file, "rb");
+    
+    efseek(f, -769, SEEK_END);
 
-	if (efgetc(f) == 12)
-	{
-		for (int i = 0; i < 256; i++)
-		{
-			efread(&colors[i].r, 1, 1, f);
-			efread(&colors[i].g, 1, 1, f);
-			efread(&colors[i].b, 1, 1, f);
-		}
-	} 
-	
-	efseek(f, 128, SEEK_SET);
-	
-	for (int i = 0; i < 320 * 200; )
-	{
-		Uint8 p = efgetc(f);
-		if ((p & 0xc0) == 0xc0)
-		{
-			i += (p & 0x3f);
-			memset(s, efgetc(f), (p & 0x3f));
-			s += (p & 0x3f);
-		} else {
-			i++;
-			*s = p;
-			s++;
-		}
-		if (i && (i % 320 == 0))
-		{
-			s += VGAScreen->pitch - 320;
-		}
-	}
-	
-	efclose(f);
+    if (efgetc(f) == 12)
+    {
+        for (int i = 0; i < 256; i++)
+        {
+            efread(&colors[i].r, 1, 1, f);
+            efread(&colors[i].g, 1, 1, f);
+            efread(&colors[i].b, 1, 1, f);
+        }
+
+        // Set the palette after loading it
+        set_palette(colors, 0, 255);
+    } 
+    
+    efseek(f, 128, SEEK_SET);
+    
+    for (int i = 0; i < 320 * 200; )
+    {
+        Uint8 p = efgetc(f);
+        if ((p & 0xc0) == 0xc0)
+        {
+            int count = (p & 0x3f);
+            i += count;
+            memset(s, efgetc(f), count);
+            s += count;
+        } else {
+            i++;
+            *s = p;
+            s++;
+        }
+        if (i && (i % 320 == 0))
+        {
+            s += VGAScreen->pitch - 320;
+        }
+    }
+    
+    efclose(f);
 }
-
